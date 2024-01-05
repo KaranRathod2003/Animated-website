7eeeb6a6acfa77ef06c7c6fe9633f6f8cc1225de
Karan Rathod <kr04391@gmail.com>
2024-01-05 17:39:28 +0530

    first phase is completed

---
+++ main.js
@@ -0,0 +1,70 @@
+// async an sync
+// console.log("hey 1")
+// setTimeout(() => {
+//     console.log("hey 2")
+// }, 12000);
+// setTimeout(() => {
+//     console.log("hey 3")
+// }, 2000);
+
+
+// promises 
+// var a =new Promise((resolve, reject) => {
+//     let n = Math.floor(Math.random()*10);
+//     if (n<5) {
+//         return resolve();
+//     } else {
+//         return reject();
+//     }
+// })
+// a.then(function(){
+//     console.log("below 5")
+// })
+// .catch(function(){
+//     console.log("above 5")
+// })
+
+
+// // async await
+// async function num() {
+//     let raw = await fetch(``);
+//     let data = await raw.json();   // agar await nahi hai to ye main stack me chala jayega
+//     console.log(data) ; 
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
